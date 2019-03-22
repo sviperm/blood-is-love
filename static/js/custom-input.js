ba8a3c4336cb4631f2314f61b20e20509c0852f6
Vladislav Safonichev <unsviperm@gmail.com>
2019-03-22 03:18:18 +0300

    All js in one file

--- static/js/custom-input.js
+++
@@ -1,5 +0,0 @@
-$('.custom-file-input').on('change', function () {
-    // var fileName = document.getElementById("id_file").files[0].name;
-    $(this).next('.form-control-file').addClass("selected");
-    // $(this).next('.form-control-file').addClass("selected").html(fileName);
-})
--- static/js/preview.js
+++ static/js/preview.js
@@ -1,35 +1,62 @@
+
 $("#id_file").on('change', function () {
+    $(this).next('.form-control-file').addClass("selected");
 
     //Get count of selected files
     var countFiles = $(this)[0].files.length;
 
     var imgPath = $(this)[0].value;
     var extn = imgPath.substring(imgPath.lastIndexOf('.') + 1).toLowerCase();
-    var image_holder = $("#image-holder");
+    var image_holder = $("#image_holder");
+    var image_list = $("#image_list");
     image_holder.empty();
+    image_list.empty();
 
+    // TODO: добавить разрешения
     if (extn == "gif" || extn == "png" || extn == "jpg" || extn == "jpeg") {
-        if (typeof (FileReader) != "undefined") {
 
-            //loop for each file selected for uploaded.
-            for (var i = 0; i < countFiles; i++) {
-
-                var reader = new FileReader();
+        //loop for each file selected for uploaded.
+        for (var i = 0; i < countFiles; i++) {
+            (function (j, self) {
+                var reader = new FileReader()
                 reader.onload = function (e) {
+                    count = countFiles - j
+                    if (count == 1) {
+                        $("<a />", {
+                            "class": "list-group-item list-group-item-action active",
+                            "href": "#list-item-" + count,
+                            text: self.files[j].name,
+                        }).appendTo(image_list);
+                    } else {
+                        $("<a />", {
+                            "class": "list-group-item list-group-item-action ",
+                            "href": "#list-item-" + count,
+                            text: self.files[j].name,
+                        }).appendTo(image_list);
+                    }
+                    $("<h4 />", {
+                        "id": "list-item-" + count,
+                        text: self.files[j].name,
+                    }).appendTo(image_holder);
                     $("<img />", {
                         "src": e.target.result,
-                        "class": "thumb-image card-img-top"
+                        "class": "scroll-img"
                     }).appendTo(image_holder);
                 }
-
-                image_holder.show();
-                reader.readAsDataURL($(this)[0].files[i]);
-            }
-
-        } else {
-            alert("This browser does not support FileReader.");
+                reader.readAsDataURL(self.files[j])
+            })(i, this);
         }
+        $('#preview_block').removeClass("d-none");
     } else {
-        alert("Pls select only images");
+        $(this).next('.form-control-file').removeClass("selected");
+        $('#preview_block').addClass("d-none");
     }
+});
+
+$('#clear_list').on('click', function () {
+    $("#id_file").val('');
+    $("#image_holder").empty();
+    $("#image_list").empty();
+    $(this).next('.form-control-file').removeClass("selected");
+    $('#preview_block').addClass("d-none");
 });
