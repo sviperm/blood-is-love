1c0a7ec897fd7ce88b577ff91e1a337c5da57c2d
Vladislav Safonichev <unsviperm@gmail.com>
2019-03-22 03:28:47 +0300

    edit urls

--- blood_of_vlad/urls.py
+++ blood_of_vlad/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('analyzer.urls')),
+    path('', include('analyzer.urls', namespace='analyzer')),
 ]
