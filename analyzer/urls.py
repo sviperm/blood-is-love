257c0916ce9025bec5370f12c1329652e376925f
Vladislav Safonichev <unsviperm@gmail.com>
2019-03-22 03:27:10 +0300

    edit urls

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = 'analyzer'
 
 urlpatterns = [
-    path('', views.upload_file, name='home'),
-    # path('analyzer/', views.analyzer, name='analyzer'),
+    path('', views.home, name='home'),
+    path('analyzer/', views.analyzer, name='analyzer'),
+    path('upload/', views.upload, name='upload'),
+    path('data/', views.data, name='data'),
+    path('about/', views.about, name='about'),
 ]
