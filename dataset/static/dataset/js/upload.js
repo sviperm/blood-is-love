522d92d2e0e63b9caa5c7886c64f91bd070f6180
Vladislav Safonichev <unsviperm@gmail.com>
2019-04-16 18:54:38 +0300

    [NEW FEATURE] Add picture to db, preview and insta delete them

--- dataset/static/dataset/js/upload.js
+++ dataset/static/dataset/js/upload.js
@@ -33,10 +33,10 @@ $(function () {
         },
 
         done: function (e, data) {
+            $(".drag-n-drop-area").addClass('d-none');
+            $(".preview-container").removeClass('d-none');
             if (data.result.is_valid) {
-                $(".preview-container").append(
-                    "<div class='item-container'><div class='image'><img class='image-style' src='" + data.result.url + "' style='margin: " + data.result.margin + "px 0;'></div><div class='image-name'>" + data.result.name + "</div><div id='delete-image-" + data.result.id + "' class='close-btn'><div class='page-control'><span class='page-control-icon smpdf_3eHO47el2SyBBY'><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'><path fill-rule='evenodd' d='M13.582 15.001l-7.875-7.88L5 6.414 6.415 5l.707.707 7.873 7.88 7.874-7.88.707-.707 1.415 1.414-.707.707-7.875 7.88 7.875 7.88.707.708-1.415 1.414-.707-.708-7.874-7.879-7.873 7.88-.707.707L5 23.589l.707-.707 7.875-7.88z'></path></svg></span></div></div></div>"
-                )
+                $(".preview-container").append(data.result.html_item)
             }
         }
 
@@ -44,18 +44,30 @@ $(function () {
 
 });
 
-$(document).ready(function () {
-    $(".close-btn").click(function () {
-        var id = $(this).attr('id');
-        $.ajax({
-            type: 'DELETE',
-            url: "{% url 'dataset:upload' %}",
-            data: { 'pk': id },
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader("X-CSRFToken", "{{ csrf_token }}");
-            },
-            success: function (response) {
+$(document).on('click', 'button.close-btn', function () {
+    var pk = $(this).data("pk");
+    var url = $(this).data("url");
+    var csrf = $(this).data("csrf");
+    var parent = $(this).parent('div.item-container');
+
+    $.ajax({
+        type: 'POST',
+        dataType: "json",
+        url: url,
+        data: {
+            'pk': pk,
+            'csrfmiddlewaretoken': csrf,
+        },
+        success: function (response) {
+            if (response.success) {
+                $(parent).remove();
+                if ($('.preview-container').children().length == 0) {
+                    $(".preview-container").addClass('d-none');
+                    $(".drag-n-drop-area").removeClass('d-none');
+                } else {
+                    // TODO: add aler message and make background red
+                }
             }
-        });
+        }
     });
 });
--- dataset/urls.py
+++ dataset/urls.py
@@ -6,5 +6,6 @@ app_name = 'dataset'
 urlpatterns = [
     path('', views.index, name='index'),
     path('upload/', views.UploadView.as_view(), name='upload'),
-    path('/image-<int:id>', views.single_image, name='single_image'),
+    path('delete_image/', views.DeleteView.as_view(), name='delete_image'),
+    path('image-<int:id>/', views.single_image, name='single_image'),
 ]
