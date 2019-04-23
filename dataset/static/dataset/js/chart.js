d2f864f02be12185d5959cad9bc93b6f23922f79
Vladislav Safonichev <unsviperm@gmail.com>
2019-04-23 21:40:55 +0300

    + chart template

--- dataset/static/dataset/js/chart.js
+++ dataset/static/dataset/js/chart.js
@@ -1,30 +1,33 @@
 var ctx = document.getElementById('pieChart').getContext('2d');
 var theHelp = Chart.helpers;
-var pieChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# клеток',
-            data: [12, 19, 3, 5, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-        }]
-    },
-    options: {
-        events: ['mousemove', 'touchstart'],
-        animation: {
-            animateRotate: false,
-            animateScale: true,
+$(window).on('load', function () {
+    var pieChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# клеток',
+                data: [12, 19, 3, 5, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+            }]
         },
-        legend: {
-            display: false,
-        }
-    },
+        options: {
+            events: ['mousemove', 'touchstart'],
+            cutoutPercentage: 40,
+            animation: {
+                animateRotate: false,
+                animateScale: true,
+            },
+            legend: {
+                display: false,
+            }
+        },
+    });
 });
