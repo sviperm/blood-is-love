ee98820a5d45e306d65811e7a6b2d0d948761491
Vladislav Safonichev <unsviperm@gmail.com>
2019-04-25 03:24:29 +0300

    + API in urls

--- blood_of_vlad/urls.py
+++ blood_of_vlad/urls.py
@@ -18,10 +18,18 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from dataset.views import DeleteView, PieChartView, UpdateCountView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('analyzer.urls', namespace='analyzer')),
     path('dataset/', include('dataset.urls', namespace='dataset')),
+    path('api/', include([
+        path('get-pie-chart/', PieChartView.as_view(), name="api_chart"),
+        path('delete-image/', DeleteView.as_view(), name="api_delete"),
+        path('update-stat/',
+             UpdateCountView.as_view(), name="api_update_count"),
+    ])),
 ]
 
 if settings.DEBUG:
