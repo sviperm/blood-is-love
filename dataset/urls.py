1c3ee55b659e15d25d1f2acf30b28fc02bddf4c5
Vladislav Safonichev <unsviperm@gmail.com>
2019-05-02 12:02:42 +0300

    = fixed whitespace

--- dataset/urls.py
+++ dataset/urls.py
@@ -7,7 +7,7 @@ app_name = 'dataset'
 urlpatterns = [
     path('', DatasetView.as_view(), name='dataset'),
     path('<str:type>/', include([
-        path('page/<int:page_num >', DatasetPagesView.as_view(), name='dataset_view'),
+        path('page/<int:page_num>', DatasetPagesView.as_view(), name='dataset_view'),
         path('image-<int:id>/', single_image, name='single_image'),
     ])),
     path('upload/', UploadView.as_view(), name='upload'),
